85ef61b672b2c55e4f5a807e651644799580fbbf
jprasanna-shipnow <jagadish.prasanna@shipnow.services>
2024-12-29 20:45:21 +0000

    dev container

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9bd0fcf8ab9024f873d5d8a38b13c22046b08696',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9bd0fcf8ab9024f873d5d8a38b13c22046b08696',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v261',
+            'version' => '261.0.0.0',
+            'reference' => '90e575937dd57c41cf7c3daa2fcfdf71cd123323',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
